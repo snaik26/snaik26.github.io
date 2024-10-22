aa7dfe26296b2c00517a7fb303f3f546732fdd53
snaik26 <130710105+snaik26@users.noreply.github.com>
2024-10-21 20:08:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//hamburger menu
+const navEl = document.querySelector('.nav-list');
+const menuEl = document.querySelector('.menu');
+menuEl.addEventListener('click', () => {
+    navEl.classList.toggle("nav--open");
+    menuEl.classList.toggle("menu--open");
+})
+
+//close hamburger menu when clicking on nav links
+const listitemEl = document.querySelector('.list-item');
+listitemEl.addEventListener('click', () => {
+    navEl.classList.remove("nav--open");
+    menuEl.classList.remove("menu--open");
+})
+
+//randomizing stars
+var stars = document.querySelectorAll(".star");
+stars.forEach(star=>{
+    star.style.top = Math.floor(Math.random()*innerHeight/2) + 'px';
+    star.style.left = Math.floor(Math.random()*innerWidth) + 'px';
+})
+
+//email contact
+function sendEmail() {
+    window.location.href = "mailto:naik.srushti@gmail.com";
+  }
+
+//example scroll function
+//function onLinkClick() {
+    //document.getElementsByTagName('h2')[3].scrollIntoView();
+    // will scroll to 4th h3 element
+  //} 
